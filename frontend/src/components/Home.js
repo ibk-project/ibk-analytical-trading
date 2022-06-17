8d01276fe0fa2663fba5bb83406d39cd70464290
Kohminchae <mc0jooo@gmail.com>
2022-06-17 17:39:56 +0900

    add: line chart

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,20 @@
 import './css/Home.css';
+import Line from './chart/Line.js';
 
 function Home() {
+  const data = [
+    { x: 1, y: 2 },
+    { x: 2, y: 3 },
+    { x: 3, y: 5 },
+    { x: 4, y: 4 },
+    { x: 5, y: 7 }
+  ]
+  const category = ["one", "two", "three", "four", "five"]
   return(
     <div>
       <div className="home-summary">
         Home
+        <Line data={data} category={category}/>
       </div>
     </div>
   );
