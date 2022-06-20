66ae6d27faf5495833e7ddb55d913c20a3b95ea7
Yunbo <ybuser@naver.com>
2022-06-20 15:43:45 +0900

    fix: DB settings, README.md, gitignore

--- README.md
+++ README.md
@@ -6,8 +6,22 @@ proxy server: nginx
 MSA Architecture  
 deploy: AWS
 
-## 실행 방법
+## 실행 방법 (Docker)
 `sudo docker-compose up`
 을 프로젝트 최상단 디렉토리에서 실행
 
 localhost:80에서 접속 가능
+
+
+## 실행 방법 (Frontend)
+frontend 폴더에 들어가서
+npm install 후
+npm run start
+
+
+
+## IBKAT 어록
+이게 Mongo~
+DBDB딥!
+Java? 뭘 잡아
+Maria DB를 활용한단 Maria
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_api.apps.RestApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -75,8 +80,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'djongo',
+        'NAME': 'test_db',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
 
@@ -105,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
