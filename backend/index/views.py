acf00a0a80f68bfdabb8f8a9912a54ca03104ec9
Taek-Lee00 <dlrlxor548@gmail.com>
2022-07-05 10:32:58 +0900

    nn

--- backend/index/views.py
+++ backend/index/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
+import json
  
 from index.models import Index
 from index.serializers import IndexSerializer
@@ -20,14 +21,35 @@ def get_index(request):
         date = request.data.get("date")
         chart_type = request.data.get("type")
 
+        # indexs = Index.objects.all()
+        # indexs_serializer = IndexSerializer(indexs, many=True)
+        # if chart_type == 'line' :
+        #     df = df[["date", "Close"]]
+
         df = fdr.DataReader(code)
-        indexs = Index.objects.all()
-        indexs_serializer = IndexSerializer(indexs, many=True)
-
         df.index = df.index.strftime('%Y-%m-%d')
-        js = df.to_json(orient='index')
-
-        #return JsonResponse(indexs_serializer.data, safe=False)
+        df['date'] = df.index
+        js = {"data": df.to_dict('records')}
+        
         return JsonResponse(js, safe=False)
+        # if null? error msg
+        # return JsonResponse(indexs_serializer.data, safe=False)
+        # return JsonResponse(js, safe=False)
 
-# Create your views here.
+
+# def get_index(request):
+#     if request.method == 'GET':
+#         print(request.data)
+#         code = request.data.get("code")
+#         date = request.data.get("date")
+#         chart_type = request.data.get("type")
+
+#         # indexs = Index.objects.all()
+#         # indexs_serializer = IndexSerializer(indexs, many=True)
+
+#         df = fdr.DataReader(code)
+#         df.index = df.index.strftime('%Y-%m-%d')
+#         df['date'] = df.index
+#         js = {"data": df.to_dict('records')}
+#         return js
+# # Create your views here.
