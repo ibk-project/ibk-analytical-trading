5a058615b710827185439ae8de0b88c31c7be14b
st42597 <st42597@gmail.com>
2022-08-04 17:35:56 +0900

    add: SingleLine chart

--- frontend/src/components/Market.js
+++ frontend/src/components/Market.js
@@ -1,9 +1,11 @@
 import './css/Market.css';
+import SingleLine from './chart/SingleLine';
 import CandleVolume from './chart/CandleVolume';
 
 function Market() {
   return (
     <div>
+      <SingleLine></SingleLine>
       <CandleVolume></CandleVolume>
     </div>
   );
