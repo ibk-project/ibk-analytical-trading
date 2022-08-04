a02c15bfa7923822d7d53895a4ad4edad43c4bf5
Yunbo <ybuser@naver.com>
2022-08-04 19:37:52 +0900

    eda sidebar edit

--- frontend/src/components/EDA.js
+++ frontend/src/components/EDA.js
@@ -1,21 +1,15 @@
+import React, { useState } from "react";
 import './css/EDA.css';
 import {Link} from 'react-router-dom';
 
 function EDA() {
-  const makeList = (data) => {
-    let retList = [];
-      for(let i = 0; i < data.length; i++) {
-        retList.push(<li key={i}><Link to={"/eda/" + data[i].code}>{data[i].name}</Link></li>);
-      }
-    return retList;
-  };
   const indexData = [
     {"name": "KOSPI", "code": "012345"},
     {"name": "KODAQ", "code": "012346"},
     {"name": "NASDAQ", "code": "412451"},
     {"name": "S&P", "code": "573442"}
   ];
-  const stockData = [    
+  const stockData = [
     {"name": "NAVER", "code": "126834"},
     {"name": "삼성전자", "code": "825343"},
     {"name": "SK하이닉스", "code": "121097"},
@@ -37,28 +31,80 @@ function EDA() {
     {"name": "석유", "code": "875654"},
     {"name": "석탄", "code": "890352"}
   ];
+  const makeList = (data) => {
+    let retList = [];
+      for(let i = 0; i < data.length; i++) {
+        retList.push(<li key={i}><Link to={"/eda/" + data[i].code}>{data[i].name}</Link></li>);
+      }
+    return retList;
+  };
   const indexList = makeList(indexData);
   const stockList = makeList(stockData);
-  const materiaList = makeList(materialData);
+  const materialList = makeList(materialData);
+
+  const [sidebarOpen, setSideBarOpen] = useState(false);
+  const handleViewSidebar = () => {
+    setSideBarOpen(!sidebarOpen);
+  };
+  const SideBar = (data) => {
+    const sidebarClass = data.isOpen ? "eda-sidebar open" : "eda-sidebar";
+    const toggleClass = data.isOpen ? "eda-sidebar-toggle open" : "eda-sidebar-toggle";
+    const [showList1, setShowList1] = useState("none");
+    const [showList2, setShowList2] = useState("none");
+    const [showList3, setShowList3] = useState("none");
+    const click1 = (e) => {
+      showList1==="none"? (
+        setShowList1("inline")
+      ) : (
+        setShowList1("none")
+      );
+    };
+    const click2 = (e) => {
+      showList2==="none"? (
+        setShowList2("inline")
+      ) : (
+        setShowList2("none")
+      );
+    };
+    const click3 = (e) => {
+      showList3==="none"? (
+        setShowList3("inline")
+      ) : (
+        setShowList3("none")
+      );
+    };
+    
+    return (
+      <span>
+        <div className={sidebarClass}>
+          <div onClick={click1}>종합지수</div>
+          <ul style={{display: showList1}}>
+            {data.indexList}
+          </ul>
+          <div onClick={click2}>주식</div>
+          <ul style={{display: showList2}}>
+            {data.stockList}
+          </ul>
+          <div onClick={click3}>원자재</div>
+          <ul style={{display: showList3}}>
+            {data.materialList}
+          </ul> 
+        </div>
+        <button onClick={data.toggleSidebar} className={toggleClass}>
+          {data.isOpen? (
+            <span>&lt;</span>
+          ): (
+            <span>&gt;</span>
+          )}
+        </button>
+      </span>
+      
+    );
+  };
+
   return(
     <div className="eda-container">
-      <div className="eda-sidebar">
-        <div>종합지수</div>
-        <ul>
-          {indexList}
-        </ul>
-        <div>주식</div>
-        <ul>
-          {stockList}
-        </ul>
-        <div>원자제</div>
-        <ul>
-          {materiaList}
-        </ul>
-      </div>
-      <div className="eda-content">
-        
-      </div>
+      <SideBar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} indexList={indexList} stockList={stockList} materialList={materialList} />
     </div>
   );
 }
