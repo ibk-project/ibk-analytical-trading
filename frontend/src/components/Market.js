2c2d5cd8454f0dd0f6492497ee6e8ba740448512
st42597 <st42597@gmail.com>
2022-08-05 16:54:11 +0900

    add: axios query from front

--- frontend/src/components/Market.js
+++ frontend/src/components/Market.js
@@ -1,13 +1,25 @@
 import './css/Market.css';
 import SingleLine from './chart/SingleLine';
 import CandleVolume from './chart/CandleVolume';
+import axios from 'axios';
 
 function Market() {
+  const data = axios.get('/api/data-management/index/get', {
+    params: {
+        "code": "KS11",
+        "date": "2020-01-06",
+        "type": "candle"   
+    }
+  });
+  console.log(data);
   return (
-    <div>
-      <SingleLine></SingleLine>
-      <CandleVolume></CandleVolume>
+  <div>
+    <div className="market-container">
+    <div>Index</div>
+    <SingleLine data={data} />
+    <CandleVolume />
     </div>
+  </div>
   );
 }
 
