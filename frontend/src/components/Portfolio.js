dc75cdc6d6d77b00053547a653f3fc50d52d034e
Kohminchae <mc0jooo@gmail.com>
2022-08-12 12:19:56 +0900

    fix: fix portfolio chart style

--- frontend/src/components/Portfolio.js
+++ frontend/src/components/Portfolio.js
@@ -53,7 +53,7 @@ function Portfolio() {
 
     data: [{
       name: 'MDD',
-      data: [0.0, -3.6, -3.0, -2.5, 0, -0.1, -4.1, -3.6, -3.3, -2.8, -0.4, 0.0]
+      data: [0.0, -3.6, -3.0, -2.5, 0, -0.1, -4.1, -3.6, -3.3, -2.8, -1.4, 0.0]
     },]
   }
   const [chartData, setChartOption] = useState({
