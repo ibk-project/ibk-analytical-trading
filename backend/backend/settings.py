584dd4536ead5bc183ade2a23b2e7c962f87df6d
Taek-Lee00 <dlrlxor548@gmail.com>
2022-08-12 16:31:21 +0900

    Add porfolio result api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'index.apps.IndexConfig',
-    'data.apps.DataConfig'
+    'data.apps.DataConfig',
+    'portfolio.apps.PortfolioConfig'
 ]
 
 MIDDLEWARE = [
