2e0f8f68275274f424b76fea743d99e2a7ef8ba1
cho-to <nicole2974@g.skku.edu>
2022-09-12 20:49:39 +0900

    change aws mongo ip

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'HOST' : 'mongo',
         'PORT' : 27017,
         'CLIENT': {
-            'host': '3.38.141.56',
+            'host': '3.34.176.224',
             'port': 27017,
             'username': 'IBK',
             'password': '1234',
--- backend/data/views.py
+++ backend/data/views.py
@@ -21,7 +21,7 @@ from pymongo import MongoClient
 from datetime import date, datetime
 
 client = MongoClient(
-        host='3.38.141.56',
+        host='3.34.176.224',
         port = 27017,
         username = 'IBK',
         password = '1234'
