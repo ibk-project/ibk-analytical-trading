8af551578d72cfe0db07a5ba0757c3e48622ff57
cho-to <nicole2974@g.skku.edu>
2022-11-23 13:39:23 +0900

    chore: fix dockerfile

--- backend/data/views.py
+++ backend/data/views.py
@@ -24,7 +24,7 @@ from pymongo import MongoClient
 from datetime import date, datetime
 
 client = MongoClient(
-        host='3.38.129.142',
+        host='3.38.41.9',
         port = 27017,
         username = 'IBK',
         password = '1234'
